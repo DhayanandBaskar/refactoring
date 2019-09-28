ed4633f5c7855918992bfd32238f88a8e8cc16ad
Dhayanand Baskar <dhayanand.baskar@gmail.com>
2019-09-28 14:06:14 +0530

    Extract function - extracts the amount calculation logic for a performance

--- example1/statement.js
+++ example1/statement.js
@@ -8,40 +8,46 @@ export function statement(invoice, plays) {
     minimumFractionDigits: 2
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = 0;
+  for (let performance of invoice.performances) {
+    const play = plays[performance.playID];
 
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(performance, play);
 
     // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(performance.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === play.type)
+      volumeCredits += Math.floor(performance.audience / 5);
 
     // print line for this order
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
-      perf.audience
+      performance.audience
     } seats)\n`;
     totalAmount += thisAmount;
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function amountFor(performance, play) {
+    let amount = 0;
+    switch (play.type) {
+      case "tragedy":
+        amount = 40000;
+        if (performance.audience > 30) {
+          amount += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        amount = 30000;
+        if (performance.audience > 20) {
+          amount += 10000 + 500 * (performance.audience - 20);
+        }
+        amount += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    return amount;
+  }
 }
